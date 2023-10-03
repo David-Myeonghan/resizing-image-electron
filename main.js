14defbfabed11394c5e226c655afc973becfa5f9
David Myeonghan Ryu <David.ryu@imagoworks.ai>
2023-10-03 18:55:07 +0900

    create main window

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log('Hello')
+const path = require('path');
+const {app, BrowserWindow} = require('electron');
+
+// darwin on Mac, win32 on Windows, linux on Linux
+const isMac = process.platform === 'darwins'
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: 'Image Resizer',
+        width: 500,
+        height: 600
+    })
+
+    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'))
+}
+
+app.whenReady().then(() => {
+    createMainWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (!isMac) {
+        app.quit()
+    }
+})
